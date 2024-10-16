1b60dfad63c4a32f2b43439c80f51f792e936410
Sinha <visinha@ieng6-201.ucsd.edu>
2024-10-15 17:57:00 -0700

    fixed errors

--- average.c
+++ average.c
@@ -24,7 +24,7 @@ int main(int argc, char *argv[]) {
     // Calculate the average of all args
     float average = sum / (float)num_args;
 
-    printf("Average: %.2f\n", average);
+    printf("Average is: %.2f\n", average);
 
     return 0;
 }
